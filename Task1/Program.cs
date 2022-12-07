d4fd2218fd90846238bd9cfa05ada67c7324f3de
Yana <nikandrova0915@gmail.com>
2022-12-07 22:51:44 +0300

    Задача1

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.Clear();
+
+void PalindFind(string stroka)
+{
+    if(stroka[0] == stroka[4] & stroka[1] == stroka[3])
+    {
+        Console.WriteLine($"Число {stroka} является полиндромом!");
+    }
+        else{Console.WriteLine($"Число {stroka} не является полиндромом!");}
+ }
+    
+string EnterData(string ver)
+{
+    
+    Console.Write("Введите число: ");
+    ver = Console.ReadLine();
+    return ver;
+   
+}  
+string palind = string.Empty;
+palind = EnterData(palind);
+PalindFind(palind);
+
+
+
